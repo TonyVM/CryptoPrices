8c1a024398c33d3c35bddbf31b470b9a54a4315d
TonyVM <antonio.valladaresmartinez@gmail.com>
2022-09-17 09:36:09 -0500

    mi primer commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const binanceApI = 'https://api.binance.com/'
+const endpointPrices = 'https://api.binance.com/api/v3/ticker/price'
+const endpointPriceChange = ''
+const dataBody = document.querySelector('#data')
+
+
+fetch(endpointPrices)
+    .then( response => response.json())
+    .then( data => renderData(data))
+    .catch( error => console.log(error))
+
+
+function renderData(_data) {
+    let row = ''
+    _data.forEach( e => {
+        row += `<tr>
+            <td class=".border-bottom">${e.symbol}</td>
+            <td class=".border-bottom">${e.price}</td>
+            </tr>`
+    });
+    dataBody.innerHTML = row
+}
